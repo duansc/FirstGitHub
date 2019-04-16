5e09a6543f678b71a7f765ac0cc49bd1fc5058fe
duansc <49681001+duansc@users.noreply.github.com>
2019-04-16 19:42:16 +0800

    Add files via upload

---
+++ TestComment.java
@@ -0,0 +1,21 @@
+
+//  输入/** 和enter就会出现文档注释
+/**
+ * 
+ * 测试注释用法。文档测试
+ * @author 超
+ * @version 1.0·
+ */
+
+
+public class TestComment {
+	
+	/**
+	 * 程序的入口
+	 * @param args 
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println("测试注释");
+	}
+}
